eb583ab163af113574699ccd7378fece7837ab40
Sswastik60 <sswastik60@gmail.com>
2025-07-24 23:36:06 +0530

    Incomplete code

--- bacwardstrianglepattern.cpp
+++ bacwardstrianglepattern.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main(){
     for (int i = 1; i < 5; i++)
     {
-        for (int j = 5; j > 0 ;j--)
+        for (int j = 1; j < i+1 ; j++)
         {
-            cout << i ;
+            cout << " _ ";
+            cout << j;
         }
-        cout << endl ;
+        cout << endl; 
     }
+    
     
     return 0 ;
     
