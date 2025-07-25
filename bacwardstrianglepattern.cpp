edc0cb49505e49682b73e66dde3acba6de42126b
Sswastik60 <sswastik60@gmail.com>
2025-07-25 23:36:05 +0530

    Reverse triangle pattern completed

--- bacwardstrianglepattern.cpp
+++ bacwardstrianglepattern.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 using namespace std;
 
+// Conceptual Program with double inner loops in functioning
+
 int main(){
     for (int i = 1; i < 5; i++)
     {
-        for (int j = 1; j < i+1 ; j++)
+
+
+        // this is for the spaces
+        // This is the space loop which is increasing by 1 increment
+        for (int j = 1; j <i+1; j++ )
         {
-            cout << " _ ";
-            cout << j;
+            cout << " ";
         }
-        cout << endl; 
+
+
+
+        // This is for the numbers which are decreasing each row
+        for (int j = 0; j < 5 - i; j++)
+        {
+            cout << i ;
+        }
+        
+        cout << endl;
     }
-    
     
     return 0 ;
     
