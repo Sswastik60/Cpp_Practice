ca051cc56ab8f82ea2af11bcb052f4348f1b635e
Sswastik60 <sswastik60@gmail.com>
2026-01-11 19:46:58 +0530

    arr 1.2 reverse an array

---
+++ reverseanarray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void revArray(int arr[],int sz){
+    int start=0;
+    int end=sz-1;
+    if (sz%2==0)
+    {
+        while (start < end)
+        {
+            int temp;
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        } 
+    }
+    else
+    {
+        while (start!=end)
+        {
+            int temp;
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp; 
+            start++;
+            end--;
+        }   
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+}
+
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int sz=6;
+    revArray(arr,sz);//here arr points to the address of the array and hence we are actually invoking the pass by reference concept
+
+}
