585fd9d3d8acc59672442126e43eab3d57fea484
Sswastik60 <sswastik60@gmail.com>
2026-09-15 00:12:31 +0530

    dll creation

---
+++ linked_list/doubly_linked_list.c
@@ -0,0 +1,13 @@
+struct node{
+    struct node *prev;
+    int data;
+    struct node *next;
+}
+
+int main(){
+    struct node *head = malloc(sizeof(struct node));
+    head->prev = NULL;
+    head->data = 10;
+    head->next = NULL;
+    
+}
