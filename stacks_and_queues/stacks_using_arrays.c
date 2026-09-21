c0a2f324747b7fbca616e653d91cfba8a829cd79
Sswastik60 <sswastik60@gmail.com>
2026-09-22 00:57:53 +0530

    stacks

--- stacks_and_queues/stacks_using_arrays.c
+++ stacks_and_queues/stacks_using_arrays.c
@@ -25,3 +25,69 @@ void pop()
     }
     printf("%d popped from stack\n", stack[top--]);
 }
+
+void peek()
+{
+    if (top == -1)
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Top element is %d\n", stack[top]);
+}
+
+void isEmpty()
+{
+    if (top == -1)
+        printf("Stack is empty\n");
+    else
+        printf("Stack is not empty\n");
+}
+
+void display()
+{
+    if (top == -1)
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Stack elements are:\n");
+    for (int i = top; i >= 0; i--)
+        printf("%d\n", stack[i]);
+}
+
+int main()
+{
+    int choice, value;
+    while (1)
+    {
+        printf("\n1. Push\n2. Pop\n3. Peek\n4. Is Empty\n5. Display\n6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter value to push: ");
+            scanf("%d", &value);
+            push(value);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            isEmpty();
+            break;
+        case 5:
+            display();
+            break;
+        case 6:
+            exit(0);
+        default:
+            printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
